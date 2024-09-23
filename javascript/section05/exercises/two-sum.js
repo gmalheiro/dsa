5d414d9f97bb0302bce43afeb41cb121827a92fb
Gabriel Malheiro <92603922+gmalheiro@users.noreply.github.com>
2024-09-23 00:35:37 +0000

    refactor:  add linear solution
    Added linear solution with big O of O(n) in the best case and renamed file to two-sum
    
    LeetCode exercise: https://leetcode.com/problems/two-sum/description/

---
+++ javascript/section05/exercises/two-sum.js
@@ -0,0 +1,42 @@
+let array1 = [1, 2, 3, 9];
+let array2 = [1, 2, 4, 4];
+
+// Using brute force approach
+const hasPairWithSumBruteForce = (array,sum) => { // Onˆ2 or quadratic time
+    for(let i = 0; i < array.length; i++) {
+        for(let j = i + 1; j < array.length; j++){
+            if(array[i] + array[j] == sum){
+                console.log('it add up');
+            }
+        }
+    }
+}
+
+const hasPairWithSum = (data, target) => { //O(n) or linear
+    let low = 0;
+    let high = data.length - 1;
+    while (low < high) {
+        let sum = data[low] + data[high];
+        if (sum == target) {
+            return [low,high];
+        } else if (sum < target) {
+            low++;  
+        } else {
+            high--; 
+        }
+    }
+    return false;
+};
+/*
+    Considering the arrays are sorted this solution would have a linear time O(n)
+
+    This solution use the double pointer technique
+
+    What this solution is doing is the following:
+    
+    - if the sum is lesser than the target it moves the low pointer one step ahead (it increments the low pointer +1 low++)
+    
+    - If the sum is greater than the target it moves the high pointer one step before (it decreases the high pointer -1 high--)
+*/
+
+console.log(hasPairWithSum(array2,8))
