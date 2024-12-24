4b0ef2834bcb84af14df3c22ed9e2a146f9ba7e4
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-24 03:23:12 -0300

    feat: add explanation about array methods

--- javascript/section06/arraysIntroduction.js
+++ javascript/section06/arraysIntroduction.js
@@ -1,4 +1,17 @@
 const strings = ['a','b','c','d'];
 // 4 * 4 = 16 bytes of storage for storing 
 
-console.log(strings[2]);
+console.log(strings[2]); //O(1)
+
+strings.push('e'); // O(1)
+
+strings.pop(); // O(1)
+
+strings.unshift('x'); // O(n)
+
+strings.splice(Math.floor(strings.length/2),0,'alien');/*
+* In the best case: O(n / 2)
+* In the worst case: O(n)
+* */
+
+console.log(strings)
